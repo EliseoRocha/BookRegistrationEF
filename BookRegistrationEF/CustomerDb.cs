e3fd7833eecafd15638ad4797e4221fbaa5cc110
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-02-09 16:35:03 -0800

    Added some CRUD functionality

---
+++ BookRegistrationEF/CustomerDb.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+    static class CustomerDb
+    {
+        public static List<Customer> GetCustomers()
+        {
+            BookRegContext context = new BookRegContext();
+
+            //SELECT * FROM Customers
+            List<Customer> allCustomers = (from c in context.Customer
+                               select c).ToList();
+
+            return allCustomers;
+        }
+
+        public static Customer AddCustomer(Customer c)
+        {
+            BookRegContext context = new BookRegContext();
+
+            context.Customer.Add(c);
+
+            context.SaveChanges();
+
+            return c;
+        }
+
+        public static void DeleteCustomer(Customer c)
+        {
+
+            BookRegContext context = new BookRegContext();
+
+            //Telling Entity Framework (EF) that the customer
+            //exists, and we want it removed from the DB
+            //context.Customer.Attach(c);
+            //context.Customer.Remove(c);
+
+            //ALTERNATIVE
+            context.Entry(c).State = EntityState.Deleted;
+
+            context.SaveChanges();
+        }
+
+        public static Customer Update(Customer c)
+        {
+            BookRegContext context = new BookRegContext();
+
+            context.Entry(c).State = EntityState.Modified;
+
+            context.SaveChanges();
+
+            return c;
+        }
+    }
+}
--- BookRegistrationEF/Form1.Designer.cs
+++ BookRegistrationEF/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.BooksComboBox = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BooksComboBox
@@ -50,11 +51,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(236, 191);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Text Customer CRUD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // AddBookButton
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.BooksComboBox);
             this.Name = "AddBookButton";
@@ -68,6 +80,7 @@
 
         private System.Windows.Forms.ComboBox BooksComboBox;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
